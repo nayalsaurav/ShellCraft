005a04d07731e47f747d3590b1cfe7bc68783685
Saurav Nayal <nayalsaurav@gmail.com>
2025-02-23 08:08:19 +0000

    REPL

--- app/main.js
+++ app/main.js
@@ -1,4 +1,5 @@
-const readline = require("readline");
+// const readline = require("readline");
+const readline = require("node:readline/promises");
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -6,14 +7,31 @@ const rl = readline.createInterface({
 });
 
 // Uncomment this block to pass the first stage
-let commands = [];
-rl.question("$ ", (answer) => {
-  if (!isCommand(answer)) {
-    console.log(`${answer}: command not found\n`);
-  } else rl.close();
-});
+// rl.question("$ ", (answer) => {
+//   if (!isCommand(answer)) {
+//     console.log(`${answer}: command not found\n`);
+//   }
+// });
 
+let commands = [];
 function isCommand(cmd) {
-  if (commands.includes(cmd)) return true;
-  return false;
+  return commands.includes(cmd);
+}
+
+async function startShell() {
+  while (true) {
+    const answer = await rl.question("$ ");
+    const shouldContinue = await executeCommand(answer);
+    if (!shouldContinue) break;
+  }
+}
+startShell();
+
+async function executeCommand(input) {
+  const args = input.trim().split(" ");
+  const command = args[0];
+  if (!isCommand(command)) {
+    console.log(`${command}: command not found`);
+  }
+  return true;
 }
