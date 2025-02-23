4316c8f121e48f6d2ce73f7e5dceef5e127466f4
Saurav Nayal <nayalsaurav@gmail.com>
2025-02-23 08:23:57 +0000

    The exit builtin

--- app/main.js
+++ app/main.js
@@ -1,17 +1,9 @@
-// const readline = require("readline");
 const readline = require("node:readline/promises");
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
-
-// Uncomment this block to pass the first stage
-// rl.question("$ ", (answer) => {
-//   if (!isCommand(answer)) {
-//     console.log(`${answer}: command not found\n`);
-//   }
-// });
 
 let commands = [];
 function isCommand(cmd) {
@@ -24,12 +16,14 @@ async function startShell() {
     const shouldContinue = await executeCommand(answer);
     if (!shouldContinue) break;
   }
+  rl.close();
 }
 startShell();
 
 async function executeCommand(input) {
   const args = input.trim().split(" ");
   const command = args[0];
+  if (command === "exit" && args[1] == "0") return false;
   if (!isCommand(command)) {
     console.log(`${command}: command not found`);
   }
